4069fd5c7ee71b6b98195dd9da200c1baa81130d
Bluetoon <kpityi83@gmail.com>
2023-06-25 17:04:42 +0200

    portfolio oldal

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+
+//change title when focus change
+document.addEventListener("visibilitychange", () => {
+
+  document.title= document.hidden ? "Kertész István: Várlak vissza!" : "Kertész István Portfólió oldala";
+});
