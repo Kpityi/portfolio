7bee96e6540f587c1206ce1f2b1957d2de88f1ca
Kpityi <kpityi83@gmail.com>
2024-02-24 23:36:53 +0100

    data from json

---
+++ js/cv.js
@@ -0,0 +1,48 @@
+const cvSkills = document.querySelector(".cv-skills");
+const languagesBox = document.querySelector(".language");
+
+const technologies = fetch("../json/skills.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const skills = data.skills;
+    const languages = data.languages;
+    skills.forEach((skill) => {
+      cvSkills.innerHTML += `
+        <div class="row m-1">
+          <span class="m-1">
+            <i class="${skill.icon} me-1"></i>
+            <span>${skill.name}</span>
+          </span>
+          <span
+            class="progress p-1"
+            role="progressbar"
+            aria-label="${skill.name}"
+            aria-valuenow="0"
+            aria-valuemin="0"
+            aria-valuemax="100">
+            <span class="progress-bar" style="width: ${skill.percent}%"></span>
+          </span>
+        </div>      
+      `;
+    });
+    languages.forEach((language) => {
+      languagesBox.innerHTML += `
+      <div class="row m-1">
+        <span class="m-1">
+          <span>${language.name}</span>
+        </span>
+        <span
+          class="progress p-1"
+          role="progressbar"
+          aria-label="${language.name}"
+          aria-valuenow="0"
+          aria-valuemin="0"
+          aria-valuemax="100">
+          <span class="progress-bar" style="width: ${language.percent}%"></span>
+        </span>
+      </div>      
+    `;
+    });
+  })
+  .catch((e) => console.log(e));
---
+++ js/index.js
@@ -0,0 +1,19 @@
+const skillBox = document.querySelector(".skill-box");
+
+const technologies = fetch("../json/technologies.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data = data.technologies;
+    data.forEach((technologi) => {
+      skillBox.innerHTML += `
+        <div class="skill-card">
+          <div class="skill-title">${technologi.name}</div>
+          <img
+            src="./img/technologi/${technologi.logo}"
+            alt="${technologi.name}"
+            title="${technologi.name}" />
+        </div>
+      `;
+    });
+  })
+  .catch((e) => console.log(e));
