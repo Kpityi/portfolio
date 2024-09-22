16feb18d4df56d221214534fe6843cc45795e096
Kpityi <kpityi83@gmail.com>
2024-09-22 14:55:13 +0200

    modified technologies

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
-const skillBox = document.querySelector(".skill-box");
+const mainSkillBox = document.querySelector(".main-skill-box");
+const otherSkillBox = document.querySelector(".other-skill-box");
 
 const technologies = fetch("../Json/technologies.json")
   .then((response) => response.json())
   .then((data) => {
     data = data.technologies;
     data.forEach((technologi) => {
-      skillBox.innerHTML += `
-        <div class="skill-card">
-          <div class="skill-title">${technologi.name}</div>
-          <img
-            src="./img/technologi/${technologi.logo}"
-            alt="${technologi.name}"
-            title="${technologi.name}" />
-        </div>
-      `;
+      if (technologi.class === "main") {
+        mainSkillBox.innerHTML += `
+          <div class="skill-card">
+            <div class="skill-title">${technologi.name}</div>
+            <img
+              src="./img/technologi/${technologi.logo}"
+              alt="${technologi.name}"
+              title="${technologi.name}" />
+          </div>
+        `;
+      } else {
+        otherSkillBox.innerHTML += `
+          <div class="skill-card">
+            <div class="skill-title">${technologi.name}</div>
+            <img
+              src="./img/technologi/${technologi.logo}"
+              alt="${technologi.name}"
+              title="${technologi.name}" />
+          </div>
+        `;
+      }
     });
   })
   .catch((e) => console.log(e));
